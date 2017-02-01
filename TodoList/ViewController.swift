905efe8aa5c649180a2e422d6696aeaf9656a214
Watcha Pon <watchapon@watchas-mac.local>
2017-02-01 01:22:41 -0800

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Watcha Pon on 2/1/17.
+//  Copyright © 2017 Indie Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
